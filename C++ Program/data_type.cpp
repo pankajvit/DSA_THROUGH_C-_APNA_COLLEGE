6099f2bc669b7a45de93b94820e4f14af99bca4b
Pankaj Rai <pankajvitmca@gmail.com>
2021-10-11 01:21:55 +0530

    git initiated and track all file and new program

---
+++ C++ Program/data_type.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    a=12; //initialisation
+    cout<<"size of int"<<sizeof(a)<<endl;
+
+    float b;
+
+    cout<<"size of float"<<sizeof(b)<<endl;
+
+    char c;
+    cout<<"size of char"<<sizeof(c)<<endl;
+
+    return 0;
+}
---
+++ C++ Program/demo.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello girls"<<endl;
+    return 0;
+}
